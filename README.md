816790459233622bac453a11c908cb7d8ddcbca0
James Reeves <jreeves@weavejester.com>
2015-08-28 05:29:37 +0100

    Change license to MIT to match Component

--- README.md
+++ README.md
@@ -10,5 +10,4 @@ FIXME
 
 Copyright © 2015 FIXME
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the MIT License.
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject suspend "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.3"]])
