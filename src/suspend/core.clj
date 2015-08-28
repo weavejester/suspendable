958ecf688da2d0413b285a622c356ce7094cd05b
James Reeves <jreeves@weavejester.com>
2015-08-28 02:06:22 +0100

    Add suspend protocol, functions and tests

--- src/suspend/core.clj
+++ src/suspend/core.clj
@@ -1,6 +1,16 @@
-(ns suspend.core)
+(ns suspend.core
+  (:require [com.stuartsierra.component :as component]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Suspendable
+  (suspend [component]))
+
+(defn suspend-or-stop [component]
+  (if (satisfies? Suspendable component)
+    (suspend component)
+    (component/stop component)))
+
+(defn suspend-or-stop-system
+  ([system]
+   (suspend-or-stop-system system (keys system)))
+  ([system component-keys]
+   (component/update-system-reverse system component-keys #'suspend-or-stop)))
--- test/suspend/core_test.clj
+++ test/suspend/core_test.clj
@@ -1,7 +1,33 @@
 (ns suspend.core-test
   (:require [clojure.test :refer :all]
-            [suspend.core :refer :all]))
+            [suspend.core :refer :all]
+            [com.stuartsierra.component :as component]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defrecord PlainComponent []
+  component/Lifecycle
+  (start [component] (assoc component :state :started))
+  (stop  [component] (assoc component :state :stopped)))
+
+(defrecord SuspendableComponent []
+  component/Lifecycle
+  (start [component] (assoc component :state :started))
+  (stop  [component] (assoc component :state :stopped))
+  Suspendable
+  (suspend [component] (assoc component :state :suspended)))
+
+(deftest test-suspend-or-stop
+  (testing "stop"
+    (let [component (->PlainComponent)]
+      (is (= (-> component component/start suspend-or-stop :state) :stopped))))
+  (testing "suspend"
+    (let [component (->SuspendableComponent)]
+      (is (= (-> component component/start suspend-or-stop :state) :suspended)))))
+
+(deftest test-suspend-or-stop-system
+  (let [system (suspend-or-stop-system
+                (component/start
+                 (component/system-map
+                  :plain (->PlainComponent)
+                  :suspendable (->SuspendableComponent))))]
+    (is (= (-> system :plain :state) :stopped))
+    (is (= (-> system :suspendable :state) :suspended))))
