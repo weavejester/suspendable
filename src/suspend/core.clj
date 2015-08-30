e98e288f9186cbf785d56b84a4f64c43c8163ad9
James Reeves <jreeves@weavejester.com>
2015-08-30 04:16:42 +0100

    Refactor with default suspend/resume implementation

--- src/suspend/core.clj
+++ src/suspend/core.clj
@@ -1,33 +1,30 @@
 (ns suspend.core
-  (:require [com.stuartsierra.component :as component]))
+  (:require [com.stuartsierra.component :as component])
+  (:import [com.stuartsierra.component SystemMap]))
 
 (defprotocol Suspendable
   (suspend [component])
   (resume [component old-component]))
 
-(defn suspend-or-stop [component]
-  (if (satisfies? Suspendable component)
-    (suspend component)
-    (component/stop component)))
-
-(defn suspend-or-stop-system
+(defn suspend-system
   ([system]
-   (suspend-or-stop-system system (keys system)))
+   (suspend-system system (keys system)))
   ([system component-keys]
-   (component/update-system-reverse system component-keys suspend-or-stop)))
+   (component/update-system-reverse system component-keys suspend)))
 
-(defn resume-or-start [component old-component]
-  (if (and (satisfies? Suspendable component) old-component)
-    (resume component old-component)
-    (component/start component)))
-
-(defn- assoc-key-metadata [system]
-  (reduce (fn [s k] (update-in s [k] vary-meta assoc ::key k)) system (keys system)))
-
-(defn resume-or-start-system
+(defn resume-system
   ([system old-system]
-   (resume-or-start-system system old-system (keys system)))
+   (resume-system system old-system (keys system)))
   ([system old-system component-keys]
-   (component/update-system (assoc-key-metadata system)
-                            component-keys
-                            #(resume-or-start % (get old-system (::key (meta %)))))))
+   (component/update-system
+    (reduce (fn [s k] (update-in s [k] vary-meta assoc ::key k)) system component-keys)
+    component-keys
+    (fn [c] (resume c (get old-system (::key (meta c))))))))
+
+(extend-protocol Suspendable
+  Object
+  (suspend [component] (component/stop component))
+  (resume [component _] (component/start component))
+  SystemMap
+  (suspend [system] (suspend-system system))
+  (resume [system old-system] (resume-system system old-system)))
--- test/suspend/core_test.clj
+++ test/suspend/core_test.clj
@@ -16,16 +16,15 @@
   (suspend [component]   (assoc component :state :suspended))
   (resume  [component _] (assoc component :state :resumed)))
 
-(deftest test-suspend-or-stop
-  (testing "stop"
-    (let [component (->PlainComponent)]
-      (is (= (-> component component/start suspend-or-stop :state) :stopped))))
-  (testing "suspend"
-    (let [component (->SuspendableComponent)]
-      (is (= (-> component component/start suspend-or-stop :state) :suspended)))))
+(deftest test-default-suspend
+  (is (= (-> (->PlainComponent) suspend :state) :stopped)))
 
-(deftest test-suspend-or-stop-system
-  (let [system (suspend-or-stop-system
+(deftest test-default-resume
+  (let [component (->PlainComponent)]
+    (is (= (-> component (resume component) :state) :started))))
+
+(deftest test-suspend-system
+  (let [system (suspend-system
                 (component/start
                  (component/system-map
                   :plain (->PlainComponent)
@@ -33,18 +32,10 @@
     (is (= (-> system :plain :state) :stopped))
     (is (= (-> system :suspendable :state) :suspended))))
 
-(deftest test-resume-or-start
-  (testing "start"
-    (let [component (->PlainComponent)]
-      (is (= (-> component (resume-or-start component) :state) :started))))
-  (testing "resume"
-    (let [component (->SuspendableComponent)]
-      (is (= (-> component (resume-or-start component) :state) :resumed)))))
-
-(deftest test-resume-or-start-system
+(deftest test-resume-system
   (let [system  (component/system-map
                  :plain (->PlainComponent)
                  :suspendable (->SuspendableComponent))
-        system' (resume-or-start-system system system)]
+        system' (resume-system system system)]
     (is (= (-> system' :plain :state) :started))
     (is (= (-> system' :suspendable :state) :resumed))))
