2485fbfafb241d2efdbfaefbe916839691476467
James Reeves <jreeves@weavejester.com>
2015-09-03 07:39:26 +0100

    Add project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject suspendable "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library that adds suspend and resume methods to Component"
+  :url "https://github.com/weavejester/suspendable"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
